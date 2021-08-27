e3065ee99a03dcb61361ea97240f62926849d65f
Zinadin Zidan <zidan.roking@gmail.com>
2021-08-27 19:13:22 +0200

    fs: Import Xiaomi changes
    Signed-off-by: Zinadin Zidan <zidan.roking@gmail.com>
    Change-Id: I210ce80e4cb337ed5ad39947e5b937f2ed7ad287
    Signed-off-by: Anush02198 <Anush.4376@gmail.com>

---
+++ initial_merge_memejito.sh
@@ -0,0 +1,11 @@
+git subtree add https://source.codeaurora.org/quic/la/platform/vendor/qcom-opensource/wlan/qca-wifi-host-cmn LA.UM.9.1.r1-11600-SMxxx0.0 --prefix=drivers/staging/qca-wifi-host-cmn/
+
+git subtree add https://source.codeaurora.org/quic/la/platform/vendor/qcom-opensource/wlan/qcacld-3.0 LA.UM.9.1.r1-11600-SMxxx0.0 --prefix=drivers/staging/qcacld-3.0/
+
+git subtree add https://source.codeaurora.org/quic/la/platform/vendor/qcom-opensource/wlan/fw-api LA.UM.9.1.r1-11600-SMxxx0.0 --prefix=drivers/staging/fw-api/
+
+git subtree add https://github.com/cryptomilk/kernel-sdfat.git master --prefix=fs/sdfat/
+
+git subtree add https://source.codeaurora.org/quic/la/platform/vendor/opensource/audio-kernel LA.UM.9.1.r1-11600-SMxxx0.0 --prefix=techpack/audio/
+
+git subtree add https://source.codeaurora.org/quic/femto5G/platform/vendor/qcom-opensource/data-kernel/ LA.UM.9.1.r1-11600-SMxxx0.0 --prefix=techpack/data/
